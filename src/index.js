7705e2ce4603883af7c45efd31af5031671fdb51
kunal.palwar <kunal.palwar@businessnext.com>
2025-04-04 19:57:32 +0530

    Added airplane routes

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ const express = require('express')
 const {ServerConfig} = require('./config')
 const router = express.Router();
 const apiRoutes = require('./routes')
-
 const app = express();
 
-router.use('/api',apiRoutes)
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 app.use(router)
+router.use('/api',apiRoutes)
 
 app.listen(ServerConfig.PORT,(err)=>{
     if(!err){
         console.log(`App is running on port ${ServerConfig.PORT}`)
+        
     }else{
         console.log(`Failed to start app server.Error : ${err.message}`)
     }
--- src/routes/v1/airplane.js
+++ src/routes/v1/airplane.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const { AirplaneController } = require('../../controllers');
 
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.status(200).json({"data":"Success"});
-})
+
+
+router.post('/',AirplaneController.createAirplane)
 
 module.exports = router;
